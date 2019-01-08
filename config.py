a0eec414e7cdddec107b2964b15f813586c10abe
urbangeek <gupta.amlan@gmail.com>
2019-01-08 17:28:47 -0500

    target change

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 configuration = {
 	'BASE_URL':'http://pnovels.net',
-	'TARGET_PATH':'/241332-a-betrayal-in-winter.html',
+	'TARGET_PATH':'/241331-a-shadow-in-summer.html',
 	'SEND_TO_KINDLE':'Y',
 	'SMTP_SERVER': 'smtp.gmail.com:587',
 	'SMTP_USER': 'xxx@gmail.com',
